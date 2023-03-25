4d4b949829b6aa3d2a4a1865d09a2a486254c9c4
romualdobarao <101812197+romualdobarao@users.noreply.github.com>
2023-03-24 18:10:13 -0700

    factory_pattern_barao.py

--- factory_pattern_barao.py
+++ factory_pattern_barao.py
@@ -1,4 +1,4 @@
-#we define an Enemy base class with properties like name, strength, speed, and attack_power, and a common attack method that prints a message to the console.
+#define ug enemy na base class
 class Enemy:
     def __init__(self, name, strength, speed, attack_power):
         self.name = name
@@ -8,7 +8,7 @@ class Enemy:
 
     def attack(self):
         print(f"{self.name} attacks with {self.attack_power} power.")
-#Then we define three subclasses of Enemy, namely Goblin, Orc, and Troll, that inherit from the base class and set their unique attributes in their constructors
+#define dayon ug tulo ma subclasses
 class Goblin(Enemy):
     def __init__(self):
         super().__init__("Goblin", 10, 5, 2)
@@ -20,7 +20,7 @@ class Orc(Enemy):
 class Troll(Enemy):
     def __init__(self):
         super().__init__("Troll", 20, 2, 5)
-#we define an EnemyFactory class that has a create_enemy method that takes an enemy_type string and returns an instance of the corresponding enemy class
+#define ug EnemyFactory na class
 class EnemyFactory:
     def create_enemy(self, enemy_type):
         if enemy_type == "Goblin":
@@ -31,12 +31,12 @@ class EnemyFactory:
             return Troll()
         else:
             raise ValueError(f"Unknown enemy type {enemy_type}.")
-#We create an instance of the factory class and use it to create instances of the specific enemy types.
+#mag create instance sa enemy factory na class
 enemy_factory = EnemyFactory()
 goblin = enemy_factory.create_enemy("Goblin")
 orc = enemy_factory.create_enemy("Orc")
 troll = enemy_factory.create_enemy("Troll")
-#When we call the attack method on the enemy instances, it prints the appropriate message to the console based on the attack_power attribute of the instance.
+#tawagon ang call na method
 goblin.attack()
 orc.attack()
 troll.attack()
